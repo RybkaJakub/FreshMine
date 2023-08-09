ded0dad8c8e91505d2d8106476fc82757368a8ba
Lak0ba <137114209+Lak0ba@users.noreply.github.com>
2023-08-09 21:30:54 +0200

    Update players.js

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,31 +1,32 @@
 var data = document.getElementsByClassName("light offline ")
-let dataLen = data.length;
-let lightdark = 'light';
+		let dataLen = data.length;
+		let lightdark = 'light';
 
-function initServerData(){
-    fetch('https://eu.mc-api.net/v3/server/ping/nikl-survival.mc.hostify.cz')
-      .then(response => response.json())
-      .then(data => handleServerStatus(data));
-  
-      function handleServerStatus(data){
-        const playerCounter = document.getElementById("player-count-nikl");
-        playerCounter.innerHTML = data.players.online;
-      }
-  }
-  window.onload = function () {
-    initServerData();
-    for (let i = 0; i < dataLen; i++) {
-        if (lightdark != 'lightdark'){
-            data[i].className = "light offline ";
-            lightdark = 'dark';
-            console.log(data.[i].className)
-            lightdark.log(data.[i].className)
-        }
-        else{
-            data[i].className = "dark offline ";
-            lightdark = 'light';
-            console.log(data.[i].className)
-            lightdark.log(data.[i].className)
-        }
-    }
-  }
+		function
+			initServerData() {
+			fetch('https://eu.mc-api.net/v3/server/ping/nikl-survival.mc.hostify.cz')
+				.then(response => response.json())
+				.then(data => handleServerStatus(data));
+
+			function handleServerStatus(data) {
+				const playerCounter = document.getElementById("player-count-nikl");
+				playerCounter.innerHTML = data.players.online;
+			}
+		}
+		window.onload = function () {
+			initServerData();
+			for (let i = 0; i < dataLen; i++) {
+				if (lightdark != 'lightdark') {
+					data[i].className = "light offline ";
+					lightdark = 'dark';
+					console.log(data[i].className)
+					lightdark.log(data[i].className)
+				}
+				else {
+					data[i].className = "dark offline ";
+					lightdark = 'light';
+					console.log(data[i].className)
+					lightdark.log(data[i].className)
+				}
+			}
+		}
